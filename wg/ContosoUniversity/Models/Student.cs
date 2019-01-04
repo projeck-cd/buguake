e4a4c6a830065d4e15d5cc01c5917064d50acc1b
projeck-cd <137968518@qq.com>
2019-01-04 11:18:27 +0800

    2019
    1-4

---
+++ wg/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        
+            public int ID { get; set; }
+            
+            public string Name { get; set; }
+            
+            public DateTime EnrollmentDate { get; set; }
+
+            public virtual ICollection<Enrollment> Enrollments { get; set; }
+        
+    }
+}
